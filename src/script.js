e37b4236239bfe81943b24998bb130a326573c3b
Aleksander Chromik <achromik@gmail.com>
2017-10-15 18:17:19 +0200

    ADD - Bulma styling

--- src/script.js
+++ src/script.js
@@ -1,11 +1,12 @@
-
+const _TOKEN_ = 'ec6b7aa56a01b04611b7c6ea9e38105ba5313723';
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
             searchText: '',
-            users: []
+            users: [],
+            count: 0
         };
     }
 
@@ -19,20 +20,41 @@ class App extends React.Component {
         const url = `https://api.github.com/search/users?q=${searchText}`;
         fetch(url)
             .then(response => response.json())
-            .then(responseJson => this.setState({users: responseJson.items}));
+            .then(responseJson => this.setState({users: responseJson.items, count: responseJson.total_count}));
     }
+
+    matchesCount = () => {
+        let len = this.state.count;
+        if (len) {
+            return this.matches = `Found ${len} results. Max 30 results will be shown.`;
+        } else {
+            return this.matches = '';
+        }
+    }    
 
     render() {
         return (
-            <div>
-                <form onSubmit={event => this.onSubmit(event)}>
-                    <label htmlFor="searchText">Search by user name</label>
-                    <input 
-                        type='text'
-                        id='searchText'
-                        onChange = {event => this.onChangeHandle(event)}
-                        value={this.state.searchText}/>
-                </form>
+            <div >
+                <div className={'column is-4 is-offset-4'}>
+                    <form  onSubmit={event => this.onSubmit(event)}>
+                        <div className={'field'}>
+                            <label className={'label'} htmlFor="searchText">Search GitHub users by user name</label>
+                            <p className={'control has-icons-left'}>
+                                <input className={'input'}
+                                    type='text'
+                                    id='searchText'
+                                    onChange = {event => this.onChangeHandle(event)}
+                                    value={this.state.searchText}/>
+                                <span className={'icon is-small is-left'}>
+                                    <i className={'fa fa-user'}></i>
+                                </span>
+                                <p className={'help has-text-white'}>
+                                    {this.matchesCount()} 
+                                </p>
+                            </p>
+                        </div>
+                    </form>
+                </div>    
                 <UsersList users={this.state.users} />
             </div>
         );
@@ -46,7 +68,7 @@ class UsersList extends React.Component {
 
     render() {
         return ( 
-            <div>
+            <div className={'column is-6 is-offset-3'}>
                 {this.users}
             </div>
         ); 
@@ -54,11 +76,45 @@ class UsersList extends React.Component {
 }
 
 class User extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            details: []
+        }
+    }
+
+    componentWillMount() {
+        const fetchObject = {
+            headers: {
+                Authorization: `token ${_TOKEN_}`
+            }
+        };
+        fetch(this.props.user.url, fetchObject)
+            .then(response => response.json())
+            .then(responseJson => this.setState({details: responseJson}));
+    }
+
     render() {
         return (
-            <div>
-                <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}} />
-                <a href={this.props.user.html_url} target='_blank'>{this.props.user.login}</a>
+            <div className={'box'}>
+                <article className={'media'}>
+                    <div className={'media-left'}>
+                        <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}} />
+                    </div>
+                    <div className={'media-content'}>
+                        <div className={'content'}>
+                            <p>
+                                <strong>Username: </strong> <a href={this.props.user.html_url} target='_blank'>{this.props.user.login}</a>
+                            </p>
+                            <p>
+                                <strong>Name: </strong> {this.state.details.name}
+                            </p>    
+                            <p>
+                                <strong>Location: </strong> {this.state.details.location}
+                            </p>    
+                        </div>
+                    </div>
+                </article>
             </div>
         );
     }
